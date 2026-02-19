968ca202f3b7bee177e814371b544795ebb07920
Andersonmia2222 <mia.anderson2026@gbwl.org>
2026-02-19 19:25:13 +0000

    4.7

--- Main.java
+++ Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      // 4.7 WRAPPER CLASSES wrap primitives into objects 
+      Integer fancyInt = new Integer(11);
+      Double fancyDoub = new(4.00);
+      Integer fancierInt = 11;
+      System.out.println(fancierInt);
+
+      int regularInt = fancierInt;
+      System.out.println(Integer.MIN_VALUE);
+      System.out.println( Integer.MAX_VALUE);
+      String steingNums = "11.0";
+      double pardedD = Double.parseDouble(stringyNums);
+      System.out.println(pardedD);
+      int parsedI = Integer.parseInt(stringNums2)
+      System.out.println(parsedI);
+
    }
 }
